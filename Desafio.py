8b9bcda64b48cab5c328c9f72bf4eee07c813e08
ferrantedenize <ferrantedenize@gmail.com>
2026-03-16 22:42:31 -0300

    Aula 2 de python - variáveis

---
+++ Desafio.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+print('Seja bem-vinda, ', nome)
+
+dia = int(input('Digite o dia que você nasceu'))
+mês = int(input('Digite o mês (1-12) em que você nasceu: '))
+ano = int(input('Digite o ano em que você nasceu'))
+
+print('Sua data de nascimento completa é:')
+
---
+++ aula02-variaveis-operadcores.py
@@ -0,0 +1,29 @@
+print('olá mundo')
+print(7+4)
+print('7' + '4') #CONCATENA STRINGS
+'''
+Comentários de
+multiplas linhas
+'''
+
+
+#Variáveis
+
+nome = 'Alexandre'
+idade = 26 #int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+
+#INPUT - SIMULAR UM FORMULÁRIO NO CMD
+nome = input('Digite seu nome: ') #str
+idade = int(input('Digite sua idade: '))
+
+print(nome, idade, peso)
+
+input(nome)
+
+
+
+
+
